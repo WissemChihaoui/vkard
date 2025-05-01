d863348cbb32f47ad7cad261cd963ff4850f9706
Wissem Chihaoui <chihahoui10@gmail.com>
2025-05-01 13:42:20 +0100

    try to fix ard border 1

--- src/sections/home/Benefits.jsx
+++ src/sections/home/Benefits.jsx
@@ -18,9 +18,7 @@ const Benefits = () => {
           {benefits.map((item) => (
             <div
               className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem] mx-auto"
-              style={{
-                backgroundImage: `url(${item.backgroundUrl})`,
-              }}
+              style={{ backgroundImage: `url("${item.backgroundUrl}")` }}
               key={item.id}
             >
               <div className="relative z-2 flex flex-col min-h-[10rem] p-[2.4rem] pointer-events-none">

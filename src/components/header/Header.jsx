cfdf4018b675ef446de3ee9caa2a65db0abf6d52
Wissem Chihaoui <chihahoui10@gmail.com>
2025-05-05 13:00:43 +0100

    fix loading spinner

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,4 @@
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { disablePageScroll, enablePageScroll } from "scroll-lock";
 
 import { useState } from "react";
@@ -40,9 +40,9 @@ const Header = () => {
       }`}
     >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
-        <a className="block w-[12rem] xl:mr-8" href="#hero">
+        <Link to={paths.root} className="block w-[12rem] xl:mr-8" href="#hero">
           <img src={brainwave} width={190} height={40} alt="Brainwave" />
-        </a>
+        </Link>
 
         <nav
           className={`${
